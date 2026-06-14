0bce091325d7266d6778e55a07c240bcd39e3618
Samm-05 <samyakmahatme@gmail.com>
2026-06-14 10:47:26 +0530

    scrapper foramtion

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-from config.settings import (
-    BASE_URL,
-    HEADERS
-)
-
+from config.settings import HEADERS
 from scraper.pipeline import Pipeline
 from utils.exporter import Exporter
 
 def main():
 
-    scraper = Pipeline(
-        HEADERS
-    )
+    scraper = Pipeline(HEADERS)
 
-    data = scraper.run(
-        BASE_URL
-    )
+    all_books = []
 
-    Exporter.to_csv(data)
+    for page in range(1, 6):
 
-    Exporter.to_json(data)
+        url = (
+            f"https://books.toscrape.com/"
+            f"catalogue/page-{page}.html"
+        )
+
+        books = scraper.run(url)
+
+        all_books.extend(books)
+
+    Exporter.to_csv(all_books)
+    Exporter.to_json(all_books)
 
     print(
-        f"Saved {len(data)} records"
+        f"Saved {len(all_books)} books"
     )
 
 if __name__ == "__main__":
