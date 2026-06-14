a0f23fd626be6a567485f6df84ff2bef32d619af
Samm-05 <samyakmahatme@gmail.com>
2026-06-14 10:49:40 +0530

    Add working book scraper

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from config.settings import HEADERS
 from scraper.pipeline import Pipeline
 from utils.exporter import Exporter
 
+import time
+import random
+
 def main():
 
     scraper = Pipeline(HEADERS)
@@ -19,11 +22,21 @@ def main():
 
         all_books.extend(books)
 
+        # Rate limiting (wait 1–3 seconds)
+        sleep_time = random.uniform(1, 3)
+
+        print(
+            f"Waiting {sleep_time:.2f} seconds..."
+        )
+
+        time.sleep(sleep_time)
+
     Exporter.to_csv(all_books)
+
     Exporter.to_json(all_books)
 
     print(
-        f"Saved {len(all_books)} books"
+        f"\nSaved {len(all_books)} books"
     )
 
 if __name__ == "__main__":
